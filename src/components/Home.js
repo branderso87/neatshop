282f49a4d51af0451a480c855df562d0e1179bc4
Brandi Anderson <branderso87@gmail.com>
2017-07-27 14:32:55 -0500

    main products component and working images

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
       <div>
         <div className='homeHeader'>
           <div>All The Neat Products You Could Ever Want</div>
-          <img src='' alt='' />
+          <img src='https://cdn.shopify.com/s/files/1/1790/3573/products/DSC_3656-small_1666x.jpg?v=1492893038' alt='' />
           <button className='bttn shopNow-bttn'>Shop Now</button>
         </div>
         <div>
--- src/components/MainProductsPage.js
+++ src/components/MainProductsPage.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import ProductsList from './ProductsList'
 
 class MainProductsPage extends Component {
 
   render () {
     return (
-      <div>MainProductsPage</div>
+      <div className='catWrapper'>
+        <aside className='catSide'>
+          <div>Categories</div>
+          <ul className='catLinks'>
+            <li><Link to=''>Apparel</Link></li>
+            <li><Link to=''>Electronics</Link></li>
+            <li><Link to=''>Health & Beauty</Link></li>
+            <li><Link to=''>Movies</Link></li>
+            <li><Link to=''>Music</Link></li>
+            <li><Link to=''>Sporting Goods</Link></li>
+            <li><Link to=''>Toys</Link></li>
+            <li><Link to=''>Video Games</Link></li>
+          </ul>
+        </aside>
+        <div>
+          <img src='https://cdn.shopify.com/s/files/1/1790/3573/products/DSC_3619-small_1666x.jpg?v=1492725457' alt='' />
+        </div>
+        <div>
+          <ProductsList />
+        </div>
+      </div>
     )
   }
 
