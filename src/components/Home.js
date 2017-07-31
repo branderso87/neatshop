7bb727b5cc4b80d0b2e8ca7950059890d69a170b
Brandi Anderson <branderso87@gmail.com>
2017-07-31 11:38:08 -0500

    revert to older stage and added fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import HomeProductList from './HomeProductList'
-import authKey from '../keys.js'
 
 class Home extends Component {
   constructor () {
     super()
     this.state = {
-      products: {}
+      products: []
     }
   }
   componentDidMount () {
-    fetch ('')
+    fetch ('http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsIneBayStores&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=BrandiAn-NeatShop-PRD-7c6cc25dc-cc3d11e7&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&affiliate.networkId=9&affiliate.trackingId=5338160172&affiliate.customId=home_products&storeName=neatthingstx&outputSelector=StoreInfo&paginationInput.entriesPerPage=6&sortOrder=EndTimeSoonest')
     .then(res => res.json())
     .then(data => this.setState({
       products: data
@@ -18,16 +17,15 @@ class Home extends Component {
     ))
   }
   render () {
-    console.log(authKey.productionKey)
     return (
       <div>
         <div className='homeHeader'>
           <div>All The Neat Products You Could Ever Want</div>
-          <img src='https://cdn.shopify.com/s/files/1/1790/3573/products/DSC_3656-small_1666x.jpg?v=1492893038' alt='' />
+          <img className='fullwidth-img' src='https://cdn.shopify.com/s/files/1/1790/3573/products/DSC_3656-small_1666x.jpg?v=1492893038' alt='' />
           <button className='bttn shopNow-bttn'>Shop Now</button>
         </div>
         <div>
-          <HomeProductList homeProducts={this.state.products}/>
+          <HomeProductList homeProducts={this.state.products} />
           <button>See More Products</button>
         </div>
       </div>
